dcf627d8c088d052b3494201208e379d62c95a56
freedom97 <claudia.fernandez1@correo.icesi.edu.co>
2019-10-15 11:06:47 -0500

    add firebase, but no it's finalize

---
+++ src/router/index.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from './../components/Login'
+import Register from './../components/Register'
+import ProfileUser from './../components/ProfileUser'
+import firebase from 'firebase'
+
+
+Vue.use(Router)
+
+const router = new Router({
+    mode: 'history',
+
+    routes: [
+        { path: '/', component: Login },
+        {
+            path: '/login', component: Login,
+            meta: {
+                requiresAuth: true
+            }
+        },
+        {
+            path: '/profileUser', component: ProfileUser,
+
+        },
+        {
+            path: '/register', component: Register,
+        },
+
+
+
+
+
+    ]
+})
+
+router.beforeEach((to, from, next) => {
+    const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
+    const currentUser = firebase.auth().currentUser
+
+    if (requiresAuth && !currentUser) {
+        next('/login')
+    } else if (requiresAuth && currentUser) {
+        next()
+    } else {
+        next()
+    }
+})
+export default router
